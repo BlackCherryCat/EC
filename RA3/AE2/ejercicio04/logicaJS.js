082720a45de1e46259538e0f6dcc1ac4414a82db
BlackCherryCat <blackcherry@danwin1210.de>
2024-11-21 19:17:02 +0100

    Ej4

--- RA3/AE2/ejercicio04/logicaJS.js
+++ RA3/AE2/ejercicio04/logicaJS.js
@@ -2,7 +2,25 @@ function verEnunciado() {
     let enunciado = "";
     document.getElementById("actividadEvaluable").innerHTML = enunciado
 }
+
 function verResolucion() {
-    let resolucion = "";
-    document.getElementById("actividadEvaluableR").innerHTML = resolucion
+    let frase = prompt("Introduzca una frase");
+    let char = prompt("Introduzca un caracter a buscar");
+    let cont = 0
+    let i = 0
+    document.getElementById("actividadEvaluableR").innerHTML = buscaLetra(frase, char, cont, i)
+}
+function buscaLetra(frase, char, cont, i) {
+    // Caso base: si 'i' ha alcanzado la longitud de la cadena
+    if (i == frase.length) {
+        return cont;
+    }
+
+    // Si el carácter en la posición i es igual al que buscamos
+    if (frase.charAt(i) == char) {
+        cont++;  // Incrementamos el contador
+    }
+
+    // Llamada recursiva: incrementamos 'i' para continuar buscando
+    return buscaLetra(frase, char, cont, i + 1);
 }
--- RA3/AE2/ejercicio12/logicaJS.js
+++
@@ -1,8 +0,0 @@
-function verEnunciado() {
-    let enunciado = "";
-    document.getElementById("actividadEvaluable").innerHTML = enunciado
-}
-function verResolucion() {
-    let resolucion = "";
-    document.getElementById("actividadEvaluableR").innerHTML = resolucion
-}
