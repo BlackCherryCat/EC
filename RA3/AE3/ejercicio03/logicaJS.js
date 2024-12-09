9ac9792ff10353b1b278c156f776b05ce3e78ed1
BlackCherryCat <blackcherry@danwin1210.de>
2024-12-10 00:44:36 +0100

    Finale fix

--- RA3/AE3/ejercicio03/logicaJS.js
+++ RA3/AE3/ejercicio03/logicaJS.js
@@ -13,15 +13,15 @@ function verResolucion() {
       <button onclick="quiniela();">Jugar La Quiniela</button>
       `
 }
-let boletos = [[]];
+let boletos = [];
 let reintegro = 0;
 function primitiva() {
     let num = parseInt(prompt("Ingrese el número de combinaciones"));
-    if (num > 0 | num <= 8) {
-        boletos = [[]];
+    if (num > 0 && num <= 8) {
+        boletos = [];
         reintegro = 0;
         for (let i = 0; i < num; i++) {
-            let numeros = []
+            let numeros = [];
             for (let j = 0; j < 6; j++) {
                 let numero = Math.floor(Math.random() * 49);
                 while (numeros.includes(numero)) {
@@ -31,7 +31,7 @@ function primitiva() {
             }
             boletos.push(numeros);
         }
-        reintegro = Math.floor(Math.random() * 10) + 1
+        reintegro = Math.floor(Math.random() * 10) + 1;
         document.getElementById('actividadEvaluableR').innerHTML = mostrarBoleto(boletos) + `<p>Reintegro: <a class="reint">${reintegro}</a></p>` + `
         <div id="ganancia">
         <button onclick="ResultadoP();">Ver ganancia</button>
@@ -39,17 +39,17 @@ function primitiva() {
         </div>
         `;
     } else {
-        document.getElementById('actividadEvaluableR').innerHTML = "Número introducido no válido"
+        document.getElementById('actividadEvaluableR').innerHTML = "Número introducido no válido";
     }
 
     function mostrarBoleto(boletos) {
-        let boletoHtml = ''
+        let boletoHtml = '';
         for (let i = 0; i < boletos.length; i++) {
-            boletoHtml += '<p>'
+            boletoHtml += '<p>';
             for (let j = 0; j < boletos[i].length; j++) {
-                boletoHtml += `<span>${boletos[i][j]}</span>`
+                boletoHtml += `<span>${boletos[i][j]}</span>`;
             }
-            boletoHtml += '</p>'
+            boletoHtml += '</p>';
         }
         return boletoHtml;
     }
@@ -59,44 +59,97 @@ function primitiva() {
 function ResultadoP() {
     let combG = [];
     let aciertos = 0;
+    let bolaciert = 0;
     let reintG = Math.floor(Math.random() * 10) + 1;
     for (let i = 0; i < 6; i++) {
-        combG.push(Math.floor(Math.random() * 49))
+        combG.push(Math.floor(Math.random() * 49));
     }
-    for (let i = 1; i < boletos.length; i++) {
+    for (let i = 0; i < boletos.length; i++) {
         for (let j = 0; j < boletos[i].length; j++) {
-            for (let k = 0; k < combG.length; k++) {
-                if (boletos[i][j] == combG[k]) {
-                    aciertos++
-                }
+            if (boletos[i][j] == combG[j]) {
+                bolaciert++;
+            } else {
+                break;
             }
         }
+        if (aciertos < bolaciert) {
+            aciertos = bolaciert;
+        }
+        bolaciert = 0;
     }
-    if (reintegro == reintG) {
-        document.getElementById('ganancia').innerHTML = `<p>${aciertos} aciertos + Reintegro <button onclick="primitiva();">Reintentar</button></p>`
-    } else {
-        document.getElementById('ganancia').innerHTML = `<p>${aciertos} aciertos <button onclick="primitiva();">Reintentar</button></p>`
+    switch (true) {
+        case aciertos == 6 && reintegro == reintG:
+            document.getElementById('ganancia').innerHTML = `<p>premio: 139.838.160. ${aciertos} aciertos + Reintegro <button onclick="primitiva();">Reintentar</button></p>`;
+            break;
+        case aciertos == 6:
+            document.getElementById('ganancia').innerHTML = `<p>premio: 13.983.816. ${aciertos} aciertos <button onclick="primitiva();">Reintentar</button></p>`;
+            break;
+        case aciertos == 5:
+            document.getElementById('ganancia').innerHTML = `<p>premio: 2.330.636. ${aciertos} aciertos <button onclick="primitiva();">Reintentar</button></p>`;
+            break;
+        case aciertos == 4:
+            document.getElementById('ganancia').innerHTML = `<p>premio: 55.491. ${aciertos} aciertos <button onclick="primitiva();">Reintentar</button></p>`;
+            break;
+        case aciertos == 3:
+            document.getElementById('ganancia').innerHTML = `<p>premio: 1.032. ${aciertos} aciertos <button onclick="primitiva();">Reintentar</button></p>`;
+            break;
+        case reintegro == reintG:
+            document.getElementById('ganancia').innerHTML = `<p>premio: 57. Reintegro. ${aciertos} aciertos <button onclick="primitiva();">Reintentar</button></p>`;
+            break;
+        default:
+            document.getElementById('ganancia').innerHTML = `<p>No premiado. ${aciertos} aciertos <button onclick="primitiva();">Reintentar</button></p>`;
+            break;
     }
 }
 
 function ResultadoPM() {
-    let intr = prompt('Introduce combinacion');
-    let combG = intr.split(' ');
-    let aciertos = 0;
-    let reintG = prompt('Introduce reintegro');
-    for (let i = 1; i < boletos.length; i++) {
-        for (let j = 0; j < boletos[i].length; j++) {
-            for (let k = 0; k < combG.length; k++) {
-                if (boletos[i][j] == combG[k]) {
-                    aciertos++
-                }
-            }
+    let combG = [];
+    for (let i = 0; i < 6; i++) {
+        let num = parseInt(prompt(`Introduzca el numero ${i + 1} de tu boleto`));
+        if (isNaN(num)) {
+            combG[i] = 0;
+        } else {
+            combG[i] = num;
         }
     }
-    if (reintegro == reintG) {
-        document.getElementById('ganancia').innerHTML = `<p>${aciertos} aciertos + Reintegro <button onclick="primitiva();">Reintentar</button></p>`
-    } else {
-        document.getElementById('ganancia').innerHTML = `<p>${aciertos} aciertos <button onclick="primitiva();">Reintentar</button></p>`
+    let aciertos = 0;
+    let bolaciert = 0;
+    let reintG = parseInt(prompt('Introduce reintegro'));
+    for (let i = 0; i < boletos.length; i++) {
+        for (let j = 0; j < boletos[i].length; j++) {
+            if (boletos[i][j] == combG[j]) {
+                bolaciert++;
+            } else {
+                break;
+            }
+        }
+        if (aciertos < bolaciert) {
+            aciertos = bolaciert;
+        }
+        bolaciert = 0;
+    }
+    switch (true) {
+        case aciertos == 6 && reintegro == reintG:
+            document.getElementById('ganancia').innerHTML = `<p>premio: 139.838.160. ${aciertos} aciertos + Reintegro <button onclick="primitiva();">Reintentar</button></p>`;
+            break;
+        case aciertos == 6:
+            document.getElementById('ganancia').innerHTML = `<p>premio: 13.983.816. ${aciertos} aciertos <button onclick="primitiva();">Reintentar</button></p>`;
+            break;
+        case aciertos == 5:
+            document.getElementById('ganancia').innerHTML = `<p>premio: 2.330.636. ${aciertos} aciertos <button onclick="primitiva();">Reintentar</button></p>`;
+            break;
+        case aciertos == 4:
+            document.getElementById('ganancia').innerHTML = `<p>premio: 55.491. ${aciertos} aciertos <button onclick="primitiva();">Reintentar</button></p>`;
+            break;
+        case aciertos == 3:
+            document.getElementById('ganancia').innerHTML = `<p>premio: 1.032. ${aciertos} aciertos <button onclick="primitiva();">Reintentar</button></p>`;
+            break;
+        case reintegro == reintG:
+            document.getElementById('ganancia').innerHTML = `<p>premio: 57. Reintegro. ${aciertos} aciertos <button onclick="primitiva();">Reintentar</button></p>`;
+            break;
+        default:
+            document.getElementById('ganancia').innerHTML = `<p>No premiado. ${aciertos} aciertos <button onclick="primitiva();">Reintentar</button></p>`;
+            break;
     }
 
 }
@@ -105,8 +158,8 @@ let pleno = [];
 let comb = 0;
 function quiniela() {
     comb = parseInt(prompt("Ingrese el número de combinaciones"));
-    if (comb > 0 | comb <= 8) {
-        boletos = [[]];
+    if (comb > 0 && comb <= 8) {
+        boletos = [];
         for (let i = 0; i < comb; i++) {
             // Generar combinación de enfrentamientos
             let combinacion = [];
@@ -123,9 +176,9 @@ function quiniela() {
         <div id="escribir">
         <button onclick="EscribirQ();">Escribir Quiniela</button>
         </div>
-        `
+        `;
     } else {
-        document.getElementById('actividadEvaluableR').innerHTML = "Número introducido no válido"
+        document.getElementById('actividadEvaluableR').innerHTML = "Número introducido no válido";
     }
 
     function mostrarBoleto(comb) {
@@ -136,17 +189,17 @@ function quiniela() {
             <p><a id="01">0</a><a id="11">1</a><a id="21">2</a><a id="m1">M</a></p>
             </div>
         `;
-        for (let i = 1; i < comb + 1; i++) {
+        for (let i = 0; i < comb; i++) {
             boletoHtml += `
             <div class="qui">
-            `
+            `;
             for (let j = 0; j < 14; j++) {
-                boletoHtml += `<p><a id="${i}${j}0">1</a><a id="${i}${j}1">X</a><a id="${i}${j}2">2</a></p>`
+                boletoHtml += `<p><a id="0${i}${j}">1</a><a id="1${i}${j}">X</a><a id="2${i}${j}">2</a></p>`
 
             }
             boletoHtml += `
             </div>
-            `
+            `;
         }
         return boletoHtml + pleno;
     }
@@ -158,136 +211,180 @@ function EscribirQ() {
         <button onclick="ResultadoQ();">Ver ganancia</button>
         <button onclick="ResultadoQM();">Ver ganancia manual</button>
         </div>
-        `
-    for (let i = 1; i < boletos.length; i++) {
+        `;
+    for (let i = 0; i < boletos.length; i++) {
         for (let j = 0; j < 14; j++) {
             if (boletos[i][j] == 0) {
-                document.getElementById('' + i + j + 0).style.backgroundColor = "rgb(223, 113, 113)"
+                document.getElementById('0' + i + j).style.backgroundColor = "rgb(223, 113, 113)";
             }
             else if (boletos[i][j] == 1) {
-                document.getElementById('' + i + j + 1).style.backgroundColor = "rgb(223, 113, 113)"
+                document.getElementById('1' + i + j).style.backgroundColor = "rgb(223, 113, 113)";
             }
             else {
-                document.getElementById('' + i + j + 2).style.backgroundColor = "rgb(223, 113, 113)"
+                document.getElementById('2' + i + j).style.backgroundColor = "rgb(223, 113, 113)";
             }
         }
 
     }
     for (let i = 0; i < pleno.length; i++) {
         if (pleno[i] == 0) {
-            document.getElementById('0' + i).style.backgroundColor = "rgb(223, 113, 113)"
+            document.getElementById('0' + i).style.backgroundColor = "rgb(223, 113, 113)";
         }
         else if (pleno[i] == 1) {
-            document.getElementById('1' + i).style.backgroundColor = "rgb(223, 113, 113)"
+            document.getElementById('1' + i).style.backgroundColor = "rgb(223, 113, 113)";
         }
         else if (pleno[i] == 2) {
-            document.getElementById('2' + i).style.backgroundColor = "rgb(223, 113, 113)"
+            document.getElementById('2' + i).style.backgroundColor = "rgb(223, 113, 113)";
         }
         else {
-            document.getElementById('m' + i).style.backgroundColor = "rgb(223, 113, 113)"
+            document.getElementById('m' + i).style.backgroundColor = "rgb(223, 113, 113)";
         }
     }
 }
 
 function ResultadoQ() {
-    let boletosG = [[]];
+    let boletosG = [];
     let aciertos = 0;
+    let bolaciert = 0;
     for (let i = 0; i < comb; i++) {
-        // Generar combinación de enfrentamientos
-        let combinacion = [];
         for (let j = 0; j < 14; j++) {
-            combinacion[j] = Math.floor(Math.random() * 3);
+            boletosG[j] = Math.floor(Math.random() * 3);
         }
-        boletosG.push(combinacion);
     }
     let plenoG = [];
     plenoG[0] = Math.round(Math.random() * 3);
     plenoG[1] = Math.round(Math.random() * 3);
-    for (let i = 1; i < boletos.length; i++) {
+    for (let i = 0; i < boletos.length; i++) {
         for (let j = 0; j < boletos[i].length; j++) {
-            if (boletos[i][j] == boletosG[i][j]) {
-                aciertos++
+            if (boletos[i][j] == boletosG[j]) {
+                bolaciert++;
             }
         }
+        if (aciertos < bolaciert) {
+            aciertos = bolaciert;
+        }
+        bolaciert = 0;
     }
     if (pleno[0] == plenoG[0] & pleno[1] == plenoG[1]) {
-        aciertos++
+        aciertos++;
     }
-    document.getElementById('ganancia').innerHTML = `<p>${aciertos} aciertos <button onclick="quiniela();">Reintentar</button></p>`
+    switch (aciertos) {
+        case 15:
+            document.getElementById('ganancia').innerHTML = `<p>premio: 76.527.504. ${aciertos} aciertos + Pleno al 15 <button onclick="quiniela();">Reintentar</button></p>`;
+            break;
+        case 14:
+            document.getElementById('ganancia').innerHTML = `<p>premio: 4.782.969. ${aciertos} aciertos <button onclick="quiniela();">Reintentar</button></p>`;
+            break;
+        case 13:
+            document.getElementById('ganancia').innerHTML = `<p>premio: 170.820. ${aciertos} aciertos <button onclick="quiniela();">Reintentar</button></p>`;
+            break;
+        case 12:
+            document.getElementById('ganancia').innerHTML = `<p>premio: 13.140. ${aciertos} aciertos <button onclick="quiniela();">Reintentar</button></p>`;
+            break;
+        case 11:
+            document.getElementById('ganancia').innerHTML = `<p>premio: 1.643. ${aciertos} aciertos <button onclick="quiniela();">Reintentar</button></p>`;
+            break;
+        case 10:
+            document.getElementById('ganancia').innerHTML = `<p>premio: 299. ${aciertos} aciertos <button onclick="quiniela();">Reintentar</button></p>`;
+            break;
+        default:
+            document.getElementById('ganancia').innerHTML = `<p>No premiado. ${aciertos} aciertos <button onclick="quiniela();">Reintentar</button></p>`;
+            break;
+    }
 }
 function ResultadoQM() {
-    let boletosG = [[]];
+    let boletosG = [];
     let aciertos = 0;
-    for (let i = 0; i < comb; i++) {
-        let cont = 0
-        let nums = [];
-        for (let j = 0; j < 14; j++) {
-            let num = 0;
-            let char = prompt("Ingrese el resultado de los enfrentamientos");
-            switch (char) {
-                case "1":
-                    num = 0
-                    break;
-                case "x":
-                    num = 1
-                    break;
-                case "2":
-                    num = 2
-                    break;
-                default:
-                    num = 0
-                    break;
-            }
-            nums[j] = num;
+    let bolaciert = 0;
+    for (let j = 0; j < 14; j++) {
+        let char = prompt("Ingrese el resultado de los enfrentamientos");
+        switch (char) {
+            case "1":
+                boletosG[j] = 0;
+                break;
+            case "x":
+                boletosG[j] = 1;
+                break;
+            case "2":
+                boletosG[j] = 2;
+                break;
+            default:
+                boletosG[j] = 0;
+                break;
         }
-        boletosG.push(nums)
     }
     let plenoG = [];
     loc = prompt("Ingrese cantidad de goles del equipo local");
     con = prompt("Ingrese cantidad de goles del equipo contrario");
     switch (loc) {
         case "0":
-            plenoG[0] = 0
+            plenoG[0] = 0;
             break;
         case "1":
-            plenoG[0] = 1
+            plenoG[0] = 1;
             break;
         case "2":
-            plenoG[0] = 2
+            plenoG[0] = 2;
             break;
         case "m":
-            plenoG[0] = 3
+            plenoG[0] = 3;
             break;
         default:
-            plenoG[0] = 0
+            plenoG[0] = 0;
             break;
     }
     switch (con) {
         case "0":
-            plenoG[1] = 0
+            plenoG[1] = 0;
             break;
         case "1":
-            plenoG[1] = 1
+            plenoG[1] = 1;
             break;
         case "2":
-            plenoG[1] = 2
+            plenoG[1] = 2;
             break;
         case "m":
-            plenoG[1] = 3
+            plenoG[1] = 3;
             break;
         default:
-            plenoG[1] = 0
+            plenoG[1] = 0;
             break;
     }
-    for (let i = 1; i < boletos.length; i++) {
+    for (let i = 0; i < boletos.length; i++) {
         for (let j = 0; j < boletos[i].length; j++) {
-            if (boletos[i][j] == boletosG[i][j]) {
-                aciertos++
+            if (boletos[i][j] == boletosG[j]) {
+                bolaciert++;
             }
         }
+        if (aciertos < bolaciert) {
+            aciertos = bolaciert;
+        }
+        bolaciert = 0;
     }
     if (pleno[0] == plenoG[0] & pleno[1] == plenoG[1]) {
-        aciertos++
+        aciertos++;
     }
-    document.getElementById('ganancia').innerHTML = `<p>${aciertos} aciertos <button onclick="quiniela();">Reintentar</button></p>`
+    switch (aciertos) {
+        case 15:
+            document.getElementById('ganancia').innerHTML = `<p>premio: 76.527.504. ${aciertos} aciertos + Pleno al 15 <button onclick="quiniela();">Reintentar</button></p>`;
+            break;
+        case 14:
+            document.getElementById('ganancia').innerHTML = `<p>premio: 4.782.969. ${aciertos} aciertos <button onclick="quiniela();">Reintentar</button></p>`;
+            break;
+        case 13:
+            document.getElementById('ganancia').innerHTML = `<p>premio: 170.820. ${aciertos} aciertos <button onclick="quiniela();">Reintentar</button></p>`;
+            break;
+        case 12:
+            document.getElementById('ganancia').innerHTML = `<p>premio: 13.140. ${aciertos} aciertos <button onclick="quiniela();">Reintentar</button></p>`;
+            break;
+        case 11:
+            document.getElementById('ganancia').innerHTML = `<p>premio: 1.643. ${aciertos} aciertos <button onclick="quiniela();">Reintentar</button></p>`;
+            break;
+        case 10:
+            document.getElementById('ganancia').innerHTML = `<p>premio: 299. ${aciertos} aciertos <button onclick="quiniela();">Reintentar</button></p>`;
+            break;
+        default:
+            document.getElementById('ganancia').innerHTML = `<p>No premiado. ${aciertos} aciertos <button onclick="quiniela();">Reintentar</button></p>`;
+            break;
+    }
 }
