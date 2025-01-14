777f439cb0ec6bd29a5c41faa93b434c41a15c09
BlackCherryCat <blackcherry@danwin1210.de>
2025-01-14 21:29:24 +0100

    AE5 ejercicio03

--- RA3/AE5/ejercicio03/logicaJS.js
+++ RA3/AE5/ejercicio03/logicaJS.js
@@ -1,14 +1,62 @@
 function popUp() {
-    window.open("screen.html", "", "width=1280,height=720,menubar=no,toolbar=no,titlebar=no,scrollbar=no");
+    window.open("screen.html", "", "width=500,menubar=no,toolbar=no,titlebar=no,scrollbar=no");
 }
 function OS() {
 
-    let os = "Desconocido";
+    let sisop = "Desconocido";
 
-    if (navigator.userAgent.indexOf("Win") != -1) os = "Windows";
-    if (navigator.userAgent.indexOf("Mac") != -1) os = "MacOS";
-    if (navigator.userAgent.indexOf("Linux") != -1) os = "Linux";
+    if (navigator.userAgent.indexOf("Win") != -1) sisop = "Windows";
+    if (navigator.userAgent.indexOf("Mac") != -1) sisop = "MacOS";
+    if (navigator.userAgent.indexOf("Linux") != -1) sisop = "Linux";
 
-    document.getElementById("but").setAttribute("value", `Descargar para ${os}`);
+    document.getElementById("but").setAttribute("value", `Descargar para ${sisop}`)
 }
-window.onload = OS;
+function browser() {
+
+    let bw = "Desconocido";
+
+    if (navigator.userAgent.indexOf("Firefox") != -1) bw = "Firefox";
+    if (navigator.userAgent.indexOf("Chrome") != -1) bw = "Chrome";
+    if (navigator.userAgent.indexOf("Mac OS X") != -1) bw = "Safari";
+
+    console.log(navigator.userAgent)
+    let collection = document.getElementsByClassName("browser")
+    for (let i = 0; i < collection.length; i++) {
+        collection[i].innerHTML = bw;
+
+    }
+
+}
+function device() {
+    let mobile = /Android|iPhone|iPad|iPod|Mobile/.test(navigator.userAgent)
+
+    if (mobile) {
+        document.getElementById('mobile').checked = true;
+    } else {
+        document.getElementById('desktop').checked = true;
+    }
+
+}
+function version() {
+    let version = "Desconocido";
+    if (navigator.userAgent.indexOf("Firefox") != -1) {
+        let versionStart = navigator.userAgent.indexOf("Firefox/") + 8;
+        version = navigator.userAgent.substring(versionStart);
+    }
+    if (navigator.userAgent.indexOf("Chrome") != -1) {
+        let versionStart = navigator.userAgent.indexOf("Chrome/") + 7;
+        version = navigator.userAgent.substring(versionStart, navigator.userAgent.indexOf(" ", versionStart));
+    }
+    if (navigator.userAgent.indexOf("Mac OS X") != -1) {
+        let versionStart = navigator.userAgent.indexOf("Safari/") + 8;
+        version = navigator.userAgent.substring(versionStart, navigator.userAgent.indexOf(" ", versionStart));
+    }
+    document.getElementById("version").innerHTML = version
+}
+function geo() {
+    navigator.geolocation.getCurrentPosition(
+        function (location) {
+            document.getElementById("lat").innerHTML = location.coords.latitude;
+            document.getElementById("lon").innerHTML = location.coords.longitude;
+        });
+}
